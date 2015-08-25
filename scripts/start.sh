90ac5f6a7441f7a8840609135ec491f9fb78c9f3
yves peeters <yves@westworld.be>
2015-08-25 13:43:40 +0200

    fixed bug for safari (rollback previous version selenium server) appium doesn't run on real ios device. did a checkout of the repo and bash reset.sh --real-safari but this failed

--- scripts/start.sh
+++
@@ -1,7 +0,0 @@
-currentdir = "`pwd`"
-server = $currentdir + "startServer.sh"
-nodes = $currentdir + "attachNodes.sh"
-admin = $currentdir + "openSeleniumAdmin.sh"
-open -a /Applications/Utilities/Terminal.app $server
-open -a /Applications/Utilities/Terminal.app $nodes
-open -a /Applications/Utilities/Terminal.app $admin
