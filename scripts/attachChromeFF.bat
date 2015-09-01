e622e57633a7c44bfef8dd635e1107d546e17103
yves peeters <yves@westworld.be>
2015-09-01 09:58:06 +0200

    Update attachChromeFF.bat
    update the command to be more generic

--- scripts/attachChromeFF.bat
+++ scripts/attachChromeFF.bat
@@ -1,3 +1,2 @@
-// adapt this line with -Dwebdriver.ie.driver=.\IEDriverServer.exe for windows
-
-java -jar jar/selenium-server-standalone-2.45.0.jar -port 8990 -role node -hub http://localhost:4444/grid/register -nodeConfig C:\Users\westworld\Desktop\selenium\selenium\scripts\nodeconfig\nodeconfigWindows.json -Dwebdriver.chrome.driver=drivers/chromedriver2.18.exe
+java -jar %~dp0\jar\selenium-server-standalone-2.45.0.jar -port 8990 -role node -hub http://IPforYourServer:4444/grid/register -nodeConfig %~dp0\nodeconfig\nodeconfigWindows.json -Dwebdriver.chrome.driver= %~dp0\drivers\chromedriver2.18.exe
+cmd /K
