b3eaf1c9bf1dc8850fab851cdbce9dabd6b62094
drBenway <yves@westworld.be>
2015-09-01 15:22:30 +0200

    updated browser versions

--- scripts/startServer.bat
+++ scripts/startServer.bat
@@ -1,1 +1,1 @@
-java -jar jar/selenium-server-standalone-2.45.0.jar -port 4444 -role hub -nodeTimeout 600
+java -jar jar/selenium-server-standalone-2.45.0.jar -port 4444 -role hub -hubConfig %~dp0\hubconfig\hubconfig.json 
