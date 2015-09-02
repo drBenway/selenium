433cc62240421bcf9c595fb2c510c16b4b1deaec
yves peeters <yves@westworld.be>
2015-09-02 13:23:06 +0200

    Update attachChromeFF.bat
    removed a space form the commandline

--- scripts/attachChromeFF.bat
+++ scripts/attachChromeFF.bat
@@ -1,2 +1,2 @@
-java -jar %~dp0\jar\selenium-server-standalone-2.45.0.jar -port 8990 -role node -hub http://IPforYourServer:4444/grid/register -nodeConfig %~dp0\nodeconfig\nodeconfigWindows.json -Dwebdriver.chrome.driver= %~dp0\drivers\chromedriver2.18.exe
+java -jar %~dp0\jar\selenium-server-standalone-2.45.0.jar -port 8990 -role node -hub http://IPforYourServer:4444/grid/register -nodeConfig %~dp0\nodeconfig\nodeconfigWindows.json -Dwebdriver.chrome.driver=%~dp0\drivers\chromedriver2.18.exe
 cmd /K
