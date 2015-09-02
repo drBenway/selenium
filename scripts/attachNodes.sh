a6801d0c9dc5ee1d49e532ce4755f5d4f95b1a1d
Yves Peeters <yves@westworld.be>
2015-09-02 15:03:06 +0200

    updated the config for mac

--- scripts/attachNodes.sh
+++ scripts/attachNodes.sh
@@ -1,3 +1,1 @@
-// adapt this line with -Dwebdriver.ie.driver=.\IEDriverServer.exe for windows
-
-java -jar jar/selenium-server-standalone-2.45.0.jar -port 8990 -role node -hub http://localhost:4444/grid/register -nodeConfig nodeconfig/nodeconfigMac.json -Dwebdriver.chrome.driver=drivers/chromedriver2.16
+java -jar jar/selenium-server-standalone-2.45.0.jar -port 8990 -role node -nodeConfig nodeconfig/nodeconfigMac.json -Dwebdriver.chrome.driver=drivers/chromedriver2.16
--- scripts/attachSafari.sh
+++ scripts/attachSafari.sh
@@ -1,2 +1,2 @@
-java -jar jar/selenium-server-standalone-2.44.0.jar -port 8991 -role node -hub http://localhost:4444/grid/register -nodeConfig /Users/Home/Desktop/selenium/scripts/nodeconfig/nodeconfigMacSafari.json -Dwebdriver.chrome.driver=drivers/selenium-safari-driver.2.45.0.jar
+java -jar jar/selenium-server-standalone-2.44.0.jar -port 8991 -role node -nodeConfig nodeconfig/nodeconfigMacSafari.json -Dwebdriver.chrome.driver=drivers/selenium-safari-driver.2.45.0.jar
 
